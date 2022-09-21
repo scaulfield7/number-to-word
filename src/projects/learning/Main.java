cb8877cbf7b3e262cdb25b7cdb1c4f753c97d7f7
scaulfield7 <scaulfield@live.ie>
2022-09-21 10:25:44 +0100

    Initial commit

---
+++ src/projects/learning/Main.java
@@ -0,0 +1,19 @@
+package projects.learning;
+
+public class Main{
+
+    public static void main(String[] args){
+        NumberToWordConverter numberToWordConverter = new NumberToWordConverter();
+        numberToWordConverter.printNumberAsWord(0);
+        numberToWordConverter.printNumberAsWord(1);
+        numberToWordConverter.printNumberAsWord(2);
+        numberToWordConverter.printNumberAsWord(3);
+        numberToWordConverter.printNumberAsWord(4);
+        numberToWordConverter.printNumberAsWord(5);
+        numberToWordConverter.printNumberAsWord(6);
+        numberToWordConverter.printNumberAsWord(7);
+        numberToWordConverter.printNumberAsWord(8);
+        numberToWordConverter.printNumberAsWord(9);
+        numberToWordConverter.printNumberAsWord(10);
+    }
+}
